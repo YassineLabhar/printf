6666f5d0af46374cd785d7d211987ad0ff6721bc
YASSINE LABHAR <124307037+YassineLabhar@users.noreply.github.com>
2023-04-24 22:07:43 +0000

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -9,34 +9,34 @@
 /**
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
- * @i: Pointer to the current position in the format string
- * @list: list of arguments
- * Return: The precision value, or -1 if it is not specified
+ * @i: Index of the current character in the format string
+ * @list: List of arguments
+ *
+ * Return: The precision
  */
-int get_precision(const char *format, int start, va_list args) {
+int get_precision(const char *format, int i, va_list list)
+{
     int precision = -1;
-    bool found_precision = false;
-    int i;
 
-    for (i = start; format[i] != '\0'; i++) {
-        if (format[i] == '.') {
-            found_precision = true;
-            continue;
-        }
+    if (format[i] != '.')
+        return (precision);
 
-        if (found_precision && isdigit(format[i])) {
-            if (precision == -1) {
-                precision = 0;
-            }
-            precision = (precision * 10) + (format[i] - '0');
-        } else if (found_precision) {
-            break;
+    precision = 0;
+    i++;
+
+    if (format[i] == '*')
+    {
+        precision = va_arg(list, int);
+        i++;
+    }
+    else
+    {
+        while (is_digit(format[i]))
+        {
+            precision = precision * 10 + (format[i] - '0');
+            i++;
         }
     }
 
-    if (precision == -1) {
-        precision = va_arg(args, int);
-    }
-
-    return precision;
+    return (precision);
 }

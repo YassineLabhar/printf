c9c8251f55aa719f8889a5404c2b1c466225d948
YASSINE LABHAR <124307037+YassineLabhar@users.noreply.github.com>
2023-04-24 22:40:09 +0000

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -1,5 +1,19 @@
 #include "main.h"
 
+ /*---------------------------------------*/
+ /* Authors:                              */
+ /* Yassine Labhar: @YassineLabhar        */
+ /* Muhammad Ali: @mu9222                 */
+ /*---------------------------------------*/
+
+/**
+ * get_precision - Calculates the precision for printing
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed
+ * @list: list of arguments
+ * Return: Precision
+ */
+
 int get_precision(const char *format, int *i, va_list list)
 {
 	int curr_i = *i + 1;

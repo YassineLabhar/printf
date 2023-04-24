7d5564242cc941ad39ba96daebf143b69fe3933e
YASSINE LABHAR <124307037+YassineLabhar@users.noreply.github.com>
2023-04-24 22:13:03 +0000

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -14,34 +14,34 @@
  *
  * Return: Precision.
  */
-int get_precision(const char *format, int i, va_list list)
+int get_precision(const char *format, int *i, va_list list)
 {
+	int curr_i = *i + 1;
 	int precision = -1;
 
-	if (format[i] != '.')
+	if (format[curr_i] != '.')
 		return (precision);
-
-	i++;
-
-	if (format[i] == '*')
-	{
-		precision = va_arg(list, int);
-		if (precision < 0)
-			precision = -1;
-		return (precision);
-	}
 
 	precision = 0;
 
-	while (is_digit(format[i]))
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
-		precision *= 10;
-		precision += (format[i] - '0');
-		i++;
+		if (is_digit(format[curr_i]))
+		{
+			precision *= 10;
+			precision += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
 	}
 
-	if (precision == 0)
-		precision = -1;
+	*i = curr_i - 1;
 
 	return (precision);
 }

40abb2d8739bc2a5646ee57ca497127d1f4fef35
YASSINE LABHAR <124307037+YassineLabhar@users.noreply.github.com>
2023-04-26 23:03:25 +0000

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -15,65 +15,58 @@ void print_buffer(char buffer[], int *buff_ind);
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int len = 0;
-    const char *str;
-    int precision;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-    va_start(args, format);
+	if (format == NULL)
+		return (-1);
 
-    while (*format != '\0')
-    {
-        if (*format == '%')
-        {
-            format++;
-            precision = 0;
+	va_start(list, format);
 
-            // check for precision
-            if (*format == '.')
-            {
-                format++;
-                while (*format >= '0' && *format <= '9')
-                {
-                    precision = (precision * 10) + (*format - '0');
-                    format++;
-                }
-            }
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%d')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
 
-            switch (*format)
-            {
-                case 's':
-                    str = va_arg(args, const char *);
-                    if (str == NULL)
-                    {
-                        len += _print("(null)");
-                        break;
-                    }
-                    
-                    // check if the string is shorter than the specified precision
-                    if (precision == 0 || precision >= _strlen(str))
-                        len += _print(str);
-                    else
-                        len += _printn(str, precision);
-                    
-                    break;
+	print_buffer(buffer, &buff_ind);
 
-                // Handle other cases here
-                
-                default:
-                    len += _print_char('%');
-                    len += _print_char(*format);
-                    break;
-            }
-        }
-        else
-        {
-            len += _print_char(*format);
-        }
-        format++;
-    }
+	va_end(list);
 
-    va_end(args);
+	return (printed_chars);
+}
 
-    return (len);
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
